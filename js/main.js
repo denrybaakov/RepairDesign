f31155d0f3c5d7822f7e17d75f6b66374c65e83b
Denis <dis-media@rambler.ru>
2020-03-11 16:44:42 +0300

    choe: js escape modal

--- js/main.js
+++ js/main.js
@@ -1,18 +1,44 @@
 document.addEventListener('DOMContentLoaded', function (event) {
 
   const modal = document.querySelector('.modal');
+  modalDialog = document.querySelector('.modal__dialog');
   const modalBtn = document.querySelectorAll('[data-toggle=modal]');
   const closeBtn = document.querySelector('.modal__close');
 
   const switchModal = () => {
     modal.classList.toggle('modal--visible');
-  }
+  };
 
   modalBtn.forEach(element => {
     element.addEventListener('click', switchModal);
   });
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   closeBtn.addEventListener('click', switchModal);
+
+
+  document.addEventListener('keydown', event => {
+    if (event.keyCode === 27) {
+      switchModal();
+    }
+  });
+
+
 
 
 
