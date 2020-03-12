544c11b6743e7f4572f054ce7b2e4454f06b4e33
Denis <dis-media@rambler.ru>
2020-03-12 13:32:30 +0300

    feat: jquery & button scrollTop

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/*
 document.addEventListener('DOMContentLoaded', function (event) {
 
   const modal = document.querySelector('.modal');
@@ -15,55 +16,49 @@ document.addEventListener('DOMContentLoaded', function (event) {
   });
 
 
-
-
-  // modal.addEventListener('click', (e) => {
-  //   if (e.target.className == '.modal__dialog') {
-  //     console.log('click');
-  //   } else if (e.target.className == 'modal--visible') {
-  //     console.log(' жуть муть дуть')
-  //   } else {
-  //     console.log('err')
-  //   }
-
-  // });
-
-
-
-
-  // console.log(modal)
-  // console.log(modalDialog)
-
-
-
-
-  // closeModal.addEventListener('click', () => {
-  //   console.log('click to ne modal')
-  // })
-
-
-
-
   closeBtn.addEventListener('click', switchModal);
 
 
   document.addEventListener('keydown', event => {
     if (event.keyCode === 27) {
-      modal.classList.remove('modal--visible')
+      modal.classList.remove('modal--visible');
     }
   });
 
+});
+*/
 
+$(document).ready(function () {
+  var modal = $('.modal'),
+    modalBtn = $('[data-toggle=modal]'),
+    closeBtn = $('.modal__close'),
+    scrollBtn = $('.scroll-top');
 
+  modalBtn.on('click', function () {
+    modal.toggleClass('modal--visible');
+  });
+  closeBtn.on('click', function () {
+    modal.toggleClass('modal--visible');
+  });
 
+  $(this).keydown(function (event) {
+    if (event.which == 27) {
+      modal.removeClass('modal--visible');
+    }
+  });
 
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 500) {
+      scrollBtn.removeClass('scroll-top__button--hidden')
+    } else {
+      scrollBtn.addClass('scroll-top__button--hidden')
+    }
+  });
 
+  scrollBtn.on('click', function (event) {
+    event.preventDefault();
+    $('html, body').animate({ scrollTop: 0 }, '300');
+  });
 
-
-
-
-
-
-
-
+  console.log(scrollBtn);
 });
