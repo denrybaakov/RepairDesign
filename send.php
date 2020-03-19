db526ed3a2bc6d1d5b83a0ed0e18dfad73f0344a
Denis <dis-media@rambler.ru>
2020-03-19 13:16:01 +0300

    add: php file send to mail

---
+++ send.php
@@ -0,0 +1,46 @@
+<?php
+  
+  $userName = $_POST['userName'];
+  $userPhone = $_POST['userPhone'];
+  $userEmail = $_POST['userEmail'];
+
+
+// Load Composer's autoloader
+require 'phpMailer/Exception.php';
+require 'phpMailer/PHPMailer.php';
+require 'phpMailer/SMTP.php';
+
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+
+try {
+    //Server settings
+    $mail->SMTPDebug = 0;                      // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'testovich.glo@gmail.com';                     // SMTP username
+    $mail->Password   = '2468097531test';                               // SMTP password
+    $mail->SMTPSecure = 'ssl';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 465;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+    //Recipients
+    $mail->setFrom('testovich.glo@gmail.com', 'Denrybaakov');
+    $mail->addAddress('rybakov22denis@yandex.ru');     // Add a recipient
+
+
+
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Новая заявка сайта';
+    $mail->Body    = "Имя пользователя: ${userName}, его телефон ${userPhone}, и его почта ${userEmail}";
+
+    $mail->send();
+    header('Location: thanks.html');
+} catch (Exception $e) {
+    echo "Ошибка отправки сообщения: {$mail->ErrorInfo}";
+}
+
+
+
+?>
